bf9039b313582294cb4ce1b057a57198df8c0de8
RPi pWerb JM <piPwJM@mail.com>
2022-11-25 12:11:56 +0100

    pi data dazu

---
+++ data_RPI/display_data.py
@@ -0,0 +1,107 @@
+from dash import dash, dcc, html, callback_context
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+import os
+import json
+import time
+import sys
+import socket
+from flask import Flask
+
+import datetime
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import sqlite3
+
+root = sys.path[0]
+
+
+def get_graph(val_range, tracefile="db.sqlite"):
+    db_path = os.path.join(root, tracefile)
+    sql_con = sqlite3.connect(db_path)
+    df = pd.read_sql_query("select * from loggings", sql_con)
+    dfLength = df.count()[0]
+    minval = val_range[0] * dfLength // 100
+    maxval = val_range[1] * dfLength // 100
+    df = df.iloc[minval:maxval]
+    sql_con.close()
+    fig = px.scatter_mapbox(
+        df,
+        lat="lat",
+        lon="lon",
+        hover_name="direction",
+        color="speed",
+        zoom=10,
+        mapbox_style="open-street-map",
+    )
+    return fig
+
+
+def traceFileList():
+    files = os.listdir()
+    tracefiles = []
+    for file in files:
+        if file.endswith(".sqlite"):
+            tracefiles.append(file)
+    return tracefiles
+
+
+fig = get_graph([0, 100])
+traceFiles = traceFileList()
+
+app = dash.Dash(
+    "PersWerb Display",
+    external_stylesheets=[dbc.themes.SUPERHERO],
+    meta_tags=[
+        {"name": "viewport"},
+        {"content": "width = device,width, initial-scale=1.0"},
+    ],
+)
+
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [  # Row 1
+                dbc.Col(
+                    html.H1(
+                        id="title_main",
+                        children="Waypoints",
+                        style={
+                            "textAlign": "center",
+                            "marginTop": 40,
+                            "marginBottom": 40,
+                            "text-decoration": "underline",
+                        },
+                    ),
+                ),
+                dcc.Dropdown(traceFiles, traceFiles[0], id="dropdown_tracefiles"),
+            ],
+            justify="center",
+        ),
+        dbc.Row(
+            [
+                dcc.Graph(
+                    id="graph_temp",
+                    figure=fig,
+                ),
+                dcc.Interval(id="interval_100s", interval=100000),
+                dcc.RangeSlider(0, 100, 5, value=[0, 100], id="range_slider"),
+            ]
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output("graph_temp", "figure"),
+    Input("interval_100s", "n_intervals"),
+    Input("range_slider", "value"),
+    Input("dropdown_tracefiles", "value"),
+)
+def update_graph(n_intervals, range_slider_values, tracefile):
+    return get_graph(range_slider_values, tracefile)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, host="0.0.0.0", port=8055)
---
+++ data_RPI/gps_logger.py
@@ -0,0 +1,330 @@
+import serial
+import io
+import time
+from datetime import datetime
+import threading
+import cv2
+from pyzbar.pyzbar import decode
+import sys
+import os
+import sqlite3
+
+import board
+import neopixel
+import logging
+
+
+class Neopixel:
+    """Für die Statusanzeige ohne Monitor"""
+
+    def __init__(self, pin, pixel_number: int) -> None:
+        """_summary_
+
+        Args:
+            pin (board.D10): Pin für die Signalleitung -> D10 verwenden
+            pixel_number (int): Anzahl LED's
+        """
+        self.pixels = neopixel.NeoPixel(pin, pixel_number, pixel_order=neopixel.RGBW)
+        for i in range(pixel_number):
+            self.pixels[i] = (0, 0, 0, 0)
+        self.pixels.show()
+
+    def update_pixel(self, pixel: int, colour):
+        """_summary_
+
+        Args:
+            pixel (int): LED Position
+            colour (_type_): Farbe der LED (Tuple(gn, rt, bl, ws))
+        """
+        self.pixels[pixel] = colour
+        self.pixels.show()
+
+
+class GPS:
+    """Hält die GPS-Daten vor"""
+
+    def __init__(self) -> None:
+        self.status = "V"
+        self.v_kmh = 0
+        self.v_kn = 0
+        self.lon = 0
+        self.lat = 0
+        self.date_time = None
+        self.dir = 0
+        self.sats_used = 0
+        self.alti = 0
+
+    @staticmethod
+    def convert_coord(NMEA_coord: str):
+        """_summary_
+
+        Args:
+            NMEA_coord (str): GPS-String von NMEA-Empfänger DDDMM.MMM
+
+        Returns:
+            floaat: Gibt des Wert in Grad uzrück
+        """
+        gm, _, m = NMEA_coord.partition(".")
+        grad = gm[:-2]
+        min = gm[-2:] + "." + m
+        coord = int(grad) + (float(min)) / 60
+        return round(coord, 6)
+
+    def out(self):
+        """Ausgabe der GPS-Daten über die Konsole"""
+        print("GPS data:")
+        print("Time", self.date_time)
+        print("Pos: ", self.lon, self.lat)
+        print("speed:", self.v_kmh, "km/h")
+        print("Dir: ", self.dir)
+        print("Alti:", self.alti)
+        print("SATs used: ", self.sats_used)
+
+
+gps = GPS()
+
+
+class QR:
+    """Infos des im Bild gefundenen QR-Codes"""
+
+    def __init__(self, data) -> None:
+        self.data = data
+        self.posx = 0
+        self.posy = 0
+
+    @staticmethod
+    def calc_code_position(frame, rect):
+        """Ermittelt die Position des Codes im Bild
+
+        Args:
+            frame (_type_): Bild in dem der Code gefunden wurde
+            rect (_type_): Rechteck-Koordinaten des Codes
+
+        Returns:
+            _type_: Position (x,y) im Bild (Werte -1 ... 1)
+        """
+        x_val = 0
+        y_val = 0
+        f_height, f_width, _ = frame.shape
+        left, top, height, width = rect
+        codeCenter = (left + width // 2, top + height // 2)
+        frameCenter = (f_width // 2, f_height // 2)
+        x_val = round((codeCenter[0] - frameCenter[0]) / frameCenter[0], 3)
+        y_val = round((codeCenter[1] - frameCenter[1]) / frameCenter[1], 3)
+        return (x_val, y_val)
+
+    def set_pos(self, pos):
+        self.posx, self.posy = pos
+
+    def debug(self):
+        """Ausgabe der Code-Daten auf der Konsole"""
+        print(20 * "*")
+        print("QR-Code Info:")
+        print(self.data)
+        print("Offset: ", self.posx, ":", self.posy)
+        print(20 * "*")
+
+
+class DL:
+    """Funktion um die Daten zu speichern"""
+
+    def __init__(self, filename="db.sqlite", timeBetween=20) -> None:
+        folder = sys.path[0]
+        self.timeBetween = timeBetween
+        self.seenCodes = {}
+        self.dbPath = os.path.join(folder, filename)
+        # initialise db
+        print(self.dbPath)
+        self.con = sqlite3.connect(self.dbPath)
+        self.cur = self.con.cursor()
+        sql_create_table = """CREATE TABLE IF NOT EXISTS "loggings" (
+                        "id"	INTEGER NOT NULL UNIQUE,
+                        "code" TEXT,
+                        "posx" NUMERIC,
+                        "posy" NUMERIC,
+                        "datetime",
+                        "lat" NUMERIC,
+                        "lon" NUMERIC,
+                        "speed" NUMERIC,
+                        "direction" NUMERIC,
+                        PRIMARY KEY("id" AUTOINCREMENT)
+                        );"""
+        self.cur.execute(sql_create_table)
+        self.con.commit()
+
+    def log_item(self, qrQode: QR):
+        if gps.status == "A":
+            if self.seenCodes.get(qrQode.data, 0) <= time.time() - self.timeBetween:
+                sql_cmd = "insert into loggings (code, posx, posy, datetime, lat, lon, speed, direction) values (?, ?, ?, ?, ?, ?, ?, ?) "
+                self.cur.execute(
+                    sql_cmd,
+                    (
+                        qrQode.data,
+                        qrQode.posx,
+                        qrQode.posy,
+                        gps.date_time,
+                        gps.lat,
+                        gps.lon,
+                        gps.v_kmh,
+                        gps.dir,
+                    ),
+                )
+                self.con.commit()
+                print("neuer Eintrag")
+            else:
+                print("zu schnell")
+        else:
+            print("GPS Signal weak")
+        self.seenCodes[qrQode.data] = time.time()
+
+    def close(self):
+        self.con.close()
+
+
+col_gn = (10, 0, 0, 0)
+col_rt = (0, 10, 0, 0)
+col_bl = (0, 0, 10, 0)
+col_ws = (0, 0, 0, 10)
+col_sw = (0, 0, 0, 0)
+
+neo = Neopixel(board.D10, 4)
+# LED 0 RPi Programm läuft
+# LED 1 Status GPS rot: nicht bereit, grün: ok
+# LED 2 blinkt bei jeder Bildauswertung
+# LED 3 blinkt bei Empfang von NMEA-Paketen
+
+
+def getGpsPos():
+    """GPS-Empfänger über USB einlesen und auswerten"""
+    ser = serial.Serial("/dev/ttyACM0", 9600, timeout=5.0)
+    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+    print("Starte GPS Auswertung")
+    while True:
+        try:
+            line = sio.readline()
+            if line.startswith("$GPRMC") or line.startswith("$GNRMC"):
+                neo.update_pixel(3, col_bl)
+                params = line.split(",")
+                gps.status = params[2]
+                if gps.status == "A":
+                    if params[4] == "N":
+                        gps.lat = GPS.convert_coord(params[3])
+                    else:
+                        gps.lat = 0 - GPS.convert_coord(params[3])
+                    if params[6] == "E":
+                        gps.lon = GPS.convert_coord(params[5])
+                    else:
+                        gps.lon = 0 - GPS.convert_coord(params[5])
+                    gps.v_kn = float(params[7])
+                    gps.v_kmh = round(gps.v_kn * 1.852, 1)
+                    try:
+                        dir = float(params[8])
+                    except:
+                        dir = None
+                    gps.dir = dir
+                    hh = int(params[1][:2])
+                    mm = int(params[1][2:4])
+                    ss = int(params[1][4:6])
+                    dd = int(params[9][:2])
+                    MM = int(params[9][2:4])
+                    yy = int(params[9][4:6]) + 2000
+                    gps.date_time = datetime(yy, MM, dd, hh, mm, ss)
+            elif line.startswith("$GPGGA") or line.startswith("$GNGGA"):
+                params = line.split(",")
+                if params[6] == "1":
+                    gps.sats_used = int(params[7])
+                    gps.alti = round(float(params[9]), 1)
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt")
+            break
+        neo.update_pixel(3, col_sw)
+
+
+def main():
+    """Routine um QR-Codes zu erkennen und die Daten dann abzulegen"""
+    gps_ready_alt = False
+    worker_gps = threading.Thread(None, getGpsPos, daemon=True)
+
+    neo.update_pixel(0, col_gn)
+    neo.update_pixel(1, col_rt)
+
+    filename = (
+        str(datetime.now())[:-7].replace("-", "").replace(":", "").replace(" ", "_")
+        + "_log.sqlite"
+    )
+    logger = DL(filename=filename)
+    worker_gps.start()
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error starting stream")
+    try:
+        while True:
+            if gps_ready_alt == False and gps.status == "A":
+                gps_ready_alt = True
+                neo.update_pixel(1, col_gn)
+            if gps_ready_alt == True and gps.status != "A":
+                gps_ready_alt = False
+                neo.update_pixel(1, col_rt)
+            neo.update_pixel(2, col_sw)
+            ret, frame = cap.read()
+            frame = cv2.resize(frame, dsize=(0, 0), fx=0.4, fy=0.4)
+            neo.update_pixel(2, col_bl)
+            codes = decode(frame)
+            for code in codes:
+                qrCode = QR(code.data.decode())
+                qrCode.set_pos(qrCode.calc_code_position(frame, code.rect))
+                # qrCode.debug()
+                logger.log_item(qrCode)
+
+    except KeyboardInterrupt:
+        print("Clean and exit program")
+        cap.release()
+        logger.close()
+
+
+def simple_logger():
+    """um einen GPS-Trace aufzuzeichnen"""
+    worker_gps = threading.Thread(None, getGpsPos, daemon=True)
+    logger = DL("trace.sqlite", 2)
+    worker_gps.start()
+    try:
+        while True:
+            qrCode = QR("logging_only")
+            logger.log_item(qrCode)
+            time.sleep(10)
+    except KeyboardInterrupt:
+        logger.close()
+
+
+def neopixel_test():
+    """Test der Neopixel-Funktion"""
+    # NeoPixels must be connected to D10, if no root rights are permitted (and SPI has to be enabled in config)
+    neo = Neopixel(board.D10, 10)
+    for i in range(10):
+        neo.update_pixel(i, (10, 0, 0, 0))  # grün
+        time.sleep(0.2)
+    for i in range(10):
+        neo.update_pixel(i, (0, 10, 0, 0))  # rot
+        time.sleep(0.2)
+    for i in range(10):
+        neo.update_pixel(i, (0, 0, 10, 0))  # blau
+        time.sleep(0.2)
+    for i in range(10):
+        neo.update_pixel(i, (0, 0, 0, 10))  # weiss
+        time.sleep(0.2)
+
+
+if __name__ == "__main__":
+    sl = False
+    neos = False
+    for arg in sys.argv:
+        if arg == "-sl":
+            sl = True
+        if arg == "-neos":
+            neos = True
+    if sl:
+        simple_logger()
+    elif neos:
+        neopixel_test()
+    else:
+        main()
